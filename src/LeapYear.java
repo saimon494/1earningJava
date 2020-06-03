f46f4a294e566b0fe3c0d91d6582bebc27237132
saimon <saimon.494@gmail.com>
2020-06-03 23:53:22 +0300

    Methods. +1

---
+++ src/LeapYear.java
@@ -0,0 +1,10 @@
+public class LeapYear {
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            if (year % 4 == 0) {
+
+            }
+        }
+        return false;
+    }
+}
